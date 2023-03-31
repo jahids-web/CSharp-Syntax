abaab7eb9bfa673fba8cad66c5ab97bcbdf9fd52
Jahid Uddin <jahiduddin922@gmail.com>
2023-04-01 02:32:48 +0600

    Write Basic Array

--- CSharp-Syntax/Arrays/Program.cs
+++ CSharp-Syntax/Arrays/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Input The Array Number: ");
+
+            var input = int.Parse(Console.ReadLine());
+            var array = new int[input];
+
+            for (var element = 0; element < input; element++) 
+            {
+                Console.Write($"Element - {element}: ");
+                array[element] = int.Parse(Console.ReadLine());
+            }
+
         }
     }
 }
