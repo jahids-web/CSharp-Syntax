0c5f9152d7eaf76ffe2a076c19744be3beaf0b60
Jahid Uddin <jahiduddin922@gmail.com>
2023-04-01 02:58:10 +0600

    Add Some Code

--- Code/Arrays/Program.cs
+++ Code/Arrays/Program.cs
@@ -2,11 +2,20 @@
 
 namespace Arrays
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Input The Array Number: ");
+
+            var input = int.Parse(Console.ReadLine());
+            var array = new int[input];
+
+            for (var element = 0; element < input; element++)
+            {
+                Console.Write($"Element - {element}: ");
+                array[element] = int.Parse(Console.ReadLine());
+            }
         }
     }
 }
