a476c3a9ef1276b8c63822c94aef18d3868856df
jahids-web <jahiduddin922@gmail.com>
2023-04-24 12:08:57 +0600

    Array Sorting

--- Code/Arrays/Program.cs
+++ Code/Arrays/Program.cs
@@ -16,6 +16,27 @@ namespace Arrays
                 Console.Write($"Element - {element}: ");
                 array[element] = int.Parse(Console.ReadLine());
             }
+
+            for (var element = 0; element < input; element++)
+            {
+                for(var sort =  element + 1; sort < input; sort++)
+                {
+                    if (array[sort] <= array[element])
+                    {
+                        var ascending = array[sort];
+                        array[sort] = array[element];
+                        array[element] = ascending;
+                    }
+                }
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("Element of array in sorted ascnding order: ");
+            for(var element = 0; element <  input; element++)
+            {
+                Console.Write($"{array[element]}");
+
+            }
+            Console.WriteLine("\n\n");
         }
     }
 }
