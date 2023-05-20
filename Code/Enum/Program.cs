419aa2e1047f5b88a1f9ac59772bbca3fb5d6213
jahids-web <jahiduddin922@gmail.com>
2023-05-20 22:15:15 +0600

    Implement Enum

---
+++ Code/Enum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        public enum Season
+        {
+            Spring,
+            Summer,
+            Autumn,
+            Winter
+        }
+
+
+        static void Main(string[] args)
+        {
+            Season weather = Season.Spring;
+            Console.WriteLine($"Integral value of = {weather}");
+
+            Season weather2 = Season.Summer;
+            Console.WriteLine($"Integral value of = {weather2}");
+
+            Season weather3 = Season.Autumn;
+            Console.WriteLine($"Integral value of = {weather3}");
+
+            Season weather4 = Season.Winter;
+            Console.WriteLine($"Integral value of = {weather4}");
+        }
+    }
+}
