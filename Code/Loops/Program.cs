2790ccf3315dca65ebf16a331a2adcfba19a7283
jahids-web <jahiduddin922@gmail.com>
2023-05-20 18:01:58 +0600

    Implement For loops

---
+++ Code/Loops/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var a = 0; a <= 20; a++) { 
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
