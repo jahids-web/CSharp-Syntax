eb8acf91514960a564597d144a79c8d80fd84042
jahids-web <jahiduddin922@gmail.com>
2023-05-20 18:18:53 +0600

    If-Else-Statement

---
+++ Code/If-Else-Statement/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace If_Else_Statement
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 5;
+            if (x == 10)
+            {
+                Console.WriteLine("x value equals to 10");
+            }
+            else if (x > 10)
+            {
+                Console.WriteLine("x value greater than 10");
+            }
+            else
+            {
+                Console.WriteLine("x value less than 10");
+            }
+            Console.WriteLine("Press Enter Key to Exit..");
+          
+        }
+    }
+}
