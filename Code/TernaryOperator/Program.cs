a4a9a72889f363320875a97cc057ef4bfb9393bd
jahids-web <jahiduddin922@gmail.com>
2023-05-20 23:09:48 +0600

    TernaryOperator

---
+++ Code/TernaryOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TernaryOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string Ages(int age) => age > 20 ? "You Can Join" : "You Can't Join";
+            //Console.Write(Ages(79));
+
+            int Result(string exm) => exm == "First" ? 100 : 0;
+            Console.WriteLine(Result("First"));
+        }
+    }
+}
